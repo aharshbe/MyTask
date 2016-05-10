851ae1371905dd0f54f137008b26c03810f73c34
aharshbe <a.catawampus@gmail.com>
2016-05-10 15:17:07 -0700

    Adds single click listener

--- MyTask/app/src/main/java/com/example/austin/mytask/MainActivity.java
+++ MyTask/app/src/main/java/com/example/austin/mytask/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -88,6 +89,13 @@ public class MainActivity extends AppCompatActivity {
                     editObject.setText("");
                 }
 
+            }
+        });
+        //Creates a onItemClick for the listView to display a toat that prompts the user to long-Press delete and object if need be
+        objectList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Long press to delete :(", Toast.LENGTH_SHORT).show();
             }
         });
     }
